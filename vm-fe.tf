56cd83173cece4106192407b80471f041deae552
Ivan Derlich <a@ivanderlich.com>
2026-08-03 18:29:05 +0000

    Add virtual machines

---
+++ vm-fe.tf
@@ -0,0 +1,22 @@
+resource "openstack_compute_instance_v2" "vm_fe" {
+  name              = "tf_bastion"
+  image_id          = data.openstack_images_image_v2.ubuntu_2404.id
+  flavor_id         = data.openstack_compute_flavor_v2.small.id
+  key_pair          = var.key_name
+  security_groups   = ["default"]
+  availability_zone = "nodos-amd-2022"
+
+  network {
+    name = "net_umstack"
+  }
+}
+
+data "openstack_networking_port_v2" "vm_fe_port" {
+  device_id  = openstack_compute_instance_v2.vm_fe.id
+  network_id = openstack_compute_instance_v2.vm_fe.network.0.uuid
+}
+
+resource "openstack_networking_floatingip_associate_v2" "vm_fe_fip" {
+  floating_ip = openstack_networking_floatingip_v2.tf_fe_fip.address
+  port_id     = data.openstack_networking_port_v2.vm_fe_port.id
+}
